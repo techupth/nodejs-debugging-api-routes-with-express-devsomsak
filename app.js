cb23f8beddfd06b4cabaed792db83a21c804ec10
devsomsak <devrleang@gmail.com>
2024-01-25 11:57:40 +0700

    fix:debuging api-route-with express, done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import assignmentRouter from "./apps/assignmentRouter.js";
 import todoRouter from "./apps/todoRouter.js";
 
 const app = express();
-const port = 4000;
+const port = 4001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +12,7 @@ app.use("/assignments", assignmentRouter);
 app.use("/todos",todoRouter);
 
 app.get("/", (req, res) => {
-  return res.send("Hello Teacher!!");
+  return res.send("Hello Teacher2!!");
 });
 
 app.listen(port, () => {
